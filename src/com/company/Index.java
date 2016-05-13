31cce289bfd3fe375f3c5884e44dad7a98944367
jack-luj <wjsc@qq.com>
2016-05-13 16:07:09 +0800

    init

---
+++ src/com/company/Index.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+/**
+ * Created by jackl on 2016/5/13.
+ */
+public class Index {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(">>>>>>>>>");
+
+
+    }
+}
